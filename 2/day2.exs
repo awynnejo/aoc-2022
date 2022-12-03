a851ae82b60c3362d5a15aa56355e135dce88477
awynnejo <89365137+awynnejo@users.noreply.github.com>
2022-12-03 21:13:16 +1000

    Adding comments

--- 2/day2.exs
+++ 2/day2.exs
@@ -1,16 +1,19 @@
-# Advent of code day 2
+# Day 2 Part 1
 # https://adventofcode.com/2022/day/2
 #
 
+# Read input
 {:ok, contents} = File.read('input.txt')
 
-hierarchy = [:rock, :scissors, :paper]
+hierarchy = [:rock, :scissors, :paper] # winning moves left to right
 move_scores = %{rock: 1, paper: 2, scissors: 3}
 game_scores = %{loss: 0, draw: 3, win: 6}
 opponent = %{"A" => :rock, "B" => :paper, "C" => :scissors}
 me = %{"X" => :rock, "Y" => :paper, "Z" => :scissors}
 outcome = %{"X" => :loss, "Y" => :draw, "Z" => :win}
 
+
+# Convert input code to my move and opponent move
 games = contents |>
   String.split("\n", trim: true) |>
   Enum.map(fn (x) ->
@@ -20,6 +23,7 @@ games = contents |>
     [opponent[List.first(x)], me[List.last(x)]]
   end)
 
+# Function that takes opponent move and my move, and returns game outcome for me (win/loss/draw)
 game_outcome = fn (op_move, my_move) ->
   if (op_move == my_move) do
     :draw
@@ -34,31 +38,17 @@ game_outcome = fn (op_move, my_move) ->
   end
 end
 
-game_outcome_part2 = fn (op_move, req_outcome) ->
-  if (req_outcome == :draw) do
-    op_move
-  else
-    op_move_hierarchy = Enum.find_index(hierarchy, fn x -> x == op_move end)
-    {:ok, my_winning_move} = Enum.fetch(hierarchy, op_move_hierarchy-1)
-    {:ok, my_losing_move} = Enum.fetch(hierarchy, rem(op_move_hierarchy+1, 2))
-    if (req_outcome == :win) do
-      my_winning_move
-    else
-      my_losing_move
-    end
-  end
-end
-
+# Determine game outcomes
 outcomes = games |>
   Enum.map(fn (x) ->
     game_outcome.(List.first(x), List.last(x))
   end)
-
+# Score game outcomes
 outcome_scored = outcomes |>
   Enum.map(fn (x) ->
     game_scores[x]
   end)
-
+# Score my moves
 move_scored = games |>
   Enum.map(fn (x) ->
     move_scores[List.last(x)]
@@ -67,5 +57,3 @@ move_scored = games |>
 
 # Part 1 answer
 total = Enum.sum(outcome_scored ++ move_scored)
-
-# TODO: Part 2
--- 2/day2part2.exs
+++ 2/day2part2.exs
@@ -1,17 +1,18 @@
-# TODO: Part 2
-#
+# Day 2 Part 2
+# https://adventofcode.com/2022/day/2
 #
 
-
+# Read input
 {:ok, contents} = File.read('input.txt')
-win_hierarchy = [:rock, :scissors, :paper]
-lose_hierarchy = Enum.reverse(win_hierarchy)
+
+win_hierarchy = [:rock, :scissors, :paper] # winning moves left to right
+lose_hierarchy = Enum.reverse(win_hierarchy) # losing moves left to right
 move_scores = %{rock: 1, paper: 2, scissors: 3}
 game_scores = %{loss: 0, draw: 3, win: 6}
 opponent = %{"A" => :rock, "B" => :paper, "C" => :scissors}
-me = %{"X" => :rock, "Y" => :paper, "Z" => :scissors}
 outcome = %{"X" => :loss, "Y" => :draw, "Z" => :win}
 
+# Convert input code to opponent move and required outcome
 games = contents |>
   String.split("\n", trim: true) |>
   Enum.map(fn (x) ->
@@ -20,8 +21,10 @@ games = contents |>
   Enum.map(fn (x) ->
     [opponent[List.first(x)], outcome[List.last(x)]]
   end)
-
-game_outcome_part2 = fn (op_move, req_outcome) ->
+# Function that takes opponent move (paper/scissors/rock) and required outcome (win/loss/draw), and returns my required move
+# e.g. iex>my_required_move.(:scissors, :win)
+# :rock
+my_required_move = fn (op_move, req_outcome) ->
   if (req_outcome == :draw) do
     op_move
   else
@@ -37,20 +40,21 @@ game_outcome_part2 = fn (op_move, req_outcome) ->
   end
 end
 
+# Determine moves I need to make
 my_moves = games |>
   Enum.map(fn (x) ->
-    game_outcome_part2.(List.first(x), List.last(x))
+    my_required_move.(List.first(x), List.last(x))
   end)
-
+# Score the outcomes
 outcome_scored = games |>
   Enum.map(fn (x) ->
     game_scores[List.last(x)]
   end)
-
+# Score the moves
 move_scored = my_moves |>
   Enum.map(fn (x) ->
     move_scores[x]
   end)
 
-
+# Sum
 total_part2 = Enum.sum(outcome_scored ++ move_scored)
